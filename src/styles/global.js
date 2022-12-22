50a2e8e920d77700a8827fccf5783a10a83720a1
dahui-sharon-kim <dahui.s.kim@gmail.com>
2022-12-22 11:43:38 +0900

    feat: global styles 설정

---
+++ src/styles/global.js
@@ -0,0 +1,83 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --blue: #252d58;
+    --light-blue: #00d0ff;
+    --text-blue: #06c;
+    --gray1: #fefefe;
+    --gray2: #ddd;
+    --gray3: #ededed;
+    --gray4: #9e9e9e;
+    --gray5: #d6d6d6;
+    --gray6: #9e9c9c;
+    --primary-100: #a9c8f8;
+    --primary-200: #98bdf6;
+    --primary-300: #7eadf4;
+    --error: #d21616;
+    --white: #fff;
+    --black: #252525;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+  
+  }
+  html {
+    @media (max-width: 600px) {
+      width: 100vw;
+      height: 100vh;
+      font-size: 1.6vw;
+    }
+  }
+  body {
+    padding: 5rem;
+    color: var(--black);
+    background-color: var(--white);
+  }
+  h1 {
+    font-size: 4rem;
+  }
+  ul,
+  li {
+    list-style: none;
+  }
+  textarea,
+  keygen,
+  select,
+  button {
+    font-family: inherit;
+    font-size: inherit;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  
+  //중앙 정렬
+  & .text-center {
+    text-align: center;
+  }
+  //커서 pointer
+  & .cursor-pointer {
+    cursor: pointer;
+  }
+  //* 복사 금지
+  & .stop-dragging {
+    -ms-user-select: none;
+    -moz-user-select: -moz-none;
+    -khtml-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+  }
+  input, textarea {
+    -webkit-user-select: text;
+    -khtml-user-select: text;
+    -moz-user-select: text;
+    -ms-user-select: text;
+    user-select: text;
+  }
+  
+`;
+
+export default GlobalStyle;
