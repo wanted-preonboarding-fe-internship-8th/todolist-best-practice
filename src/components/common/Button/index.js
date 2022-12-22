492ca7d95924ba44ded61f59e71f673791d19909
dahui-sharon-kim <dahui.s.kim@gmail.com>
2022-12-22 11:47:41 +0900

    feat: 공통component Button과 Input

---
+++ src/components/common/Button/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.div`
+  width: 282px;
+  height: 45px;
+  margin-top: 10px;
+  border-radius: 10px;
+  background: linear-gradient(to right, #44adff, #045de9);
+  outline: none;
+  color: var(--white);
+  font-size: 16px;
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  & .disabled {
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    cursor: auto;
+    color: var(--gray2);
+    background-color: var(--gray3);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export default function Button({
+  text,
+  className = '',
+  disabled = false,
+  onClick,
+}) {
+  return (
+    <StyledButton className={className} onClick={onClick} disabled={disabled}>
+      <div className={disabled ? 'disabled' : null}>{text}</div>
+    </StyledButton>
+  );
+}
---
+++ src/components/common/Input/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledInput = styled.div`
+  width: 280px;
+  & .property {
+    margin-right: 10px;
+  }
+  & .input {
+    width: 280px;
+    height: 45px;
+    padding-right: 4px;
+    background-color: var(--white);
+    font-size: 16px;
+    border: 1px solid var(--gray5);
+    border-radius: 6px;
+    ::placeholder {
+      color: var(--gray6);
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+  & .warning {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px 0;
+    font-size: 14px;
+    line-height: 1.2;
+    color: var(--error);
+    font-size: 1em;
+  }
+`;
+
+export default function Input({
+  className,
+  value,
+  type,
+  placeholder = '',
+  warningText,
+  disabled,
+  name,
+  onClick,
+  onChange,
+}) {
+  return (
+    <StyledInput warningText={warningText} className={`${className}`}>
+      <input
+        disabled={disabled}
+        placeholder={placeholder}
+        type={type}
+        className="input"
+        value={value}
+        onChange={onChange}
+        onClick={onClick}
+        name={name}
+      />
+      {warningText && <div className="warning">{warningText}</div>}
+    </StyledInput>
+  );
+}
