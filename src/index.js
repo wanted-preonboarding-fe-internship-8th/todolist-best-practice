86959fe6eac251e6fd7f974120b7ab825b5f7a3c
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-22 13:21:07 +0900

    refactor: import 경로 수정

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
+import GlobalStyle from './assets/styles/global';
 import router from './router';
-import GlobalStyle from './styles/global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
