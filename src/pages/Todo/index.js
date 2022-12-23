ff71688466cd0572ab8038cd77c3bf8b27013f60
yongbeomKim1 <dltmfrl600@gmail.com>
2022-12-23 12:29:10 +0900

    fix: 구조 변경 및 layout 수정

--- src/pages/Todo/index.js
+++ src/pages/Todo/index.js
@@ -1,12 +1,13 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
 
-import { createTodo, getTodos, updateTodo, deleteTodo } from '../api/todo';
-import Button from '../components/common/Button';
-import Input from '../components/common/Input';
-import Modal from '../components/common/Modal';
-import TodoItem from '../components/todo/TodoItem';
+import { createTodo, getTodos, updateTodo, deleteTodo } from '../../api/todo';
+import Button from '../../components/common/Button';
+import Input from '../../components/common/Input';
+import Modal from '../../components/common/Modal';
+import TodoItem from '../../components/todo/TodoItem';
+
+import { Root } from './style';
 
 export default function Todo() {
   const [todoInputValue, setTodoInputValue] = useState({
@@ -111,59 +112,66 @@ export default function Todo() {
 
   return (
     <Root>
-      <div className="todo-input-wrapper">
-        <Input
-          name="todo"
-          type="text"
-          width="450px"
-          value={todoInputValue.post}
-          onChange={(e) =>
-            setTodoInputValue({ ...todoInputValue, post: e.target.value })
-          }
-          onKeyPress={(e) =>
-            e.key === 'Enter' && onCreateTodo(todoInputValue.post)
-          }
-          placeholder="항목을 추가하세요"
-        />
-        <Button
-          onClick={() => onCreateTodo(todoInputValue.post)}
-          width="45px"
-          height="45px"
-          margin="0 0 0 10px"
-          background=""
-          color="var(--primary-400)"
-          text="추가"
-        />
-      </div>
-      <ul className="todo-list">
-        {todos?.map((todo) => {
-          return (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              isEditing={isEditTodoId === todo.id}
-              editValue={todoInputValue.update}
-              onCancelEdit={() => setIsEditTodoId()}
-              onChangeEditStatus={() => onChangeEditStatus(todo.id, todo.todo)}
-              onChangeEditValue={(e) =>
-                setTodoInputValue({ ...todoInputValue, update: e.target.value })
-              }
-              onUpdateTodo={onUpdateTodo}
-              onDeleteTodo={() => onDeleteTodo(todo.id)}
-            />
-          );
-        })}
-      </ul>
-      <div className="logout">
-        <Button
-          onClick={() => logout()}
-          width="100px"
-          height="45px"
-          margin="0 0 0 10px"
-          background="none"
-          color="var(--gray6)"
-          text="로그아웃"
-        />
+      <div className="inner">
+        <div className="todo-input-wrapper">
+          <Input
+            name="todo"
+            type="text"
+            width="450px"
+            value={todoInputValue.post}
+            onChange={(e) =>
+              setTodoInputValue({ ...todoInputValue, post: e.target.value })
+            }
+            onKeyPress={(e) =>
+              e.key === 'Enter' && onCreateTodo(todoInputValue.post)
+            }
+            placeholder="항목을 추가하세요"
+          />
+          <Button
+            onClick={() => onCreateTodo(todoInputValue.post)}
+            width="45px"
+            height="45px"
+            margin="0 0 0 10px"
+            background=""
+            color="var(--primary-400)"
+            text="추가"
+          />
+        </div>
+        <ul className="todo-list">
+          {todos?.map((todo) => {
+            return (
+              <TodoItem
+                key={todo.id}
+                todo={todo}
+                isEditing={isEditTodoId === todo.id}
+                editValue={todoInputValue.update}
+                onCancelEdit={() => setIsEditTodoId()}
+                onChangeEditStatus={() =>
+                  onChangeEditStatus(todo.id, todo.todo)
+                }
+                onChangeEditValue={(e) =>
+                  setTodoInputValue({
+                    ...todoInputValue,
+                    update: e.target.value,
+                  })
+                }
+                onUpdateTodo={onUpdateTodo}
+                onDeleteTodo={() => onDeleteTodo(todo.id)}
+              />
+            );
+          })}
+        </ul>
+        <div className="logout">
+          <Button
+            onClick={() => logout()}
+            width="100px"
+            height="45px"
+            margin="0 0 0 10px"
+            background="none"
+            color="var(--gray6)"
+            text="로그아웃"
+          />
+        </div>
       </div>
       {isModalOpen.open && (
         <Modal
@@ -175,33 +183,3 @@ export default function Todo() {
     </Root>
   );
 }
-
-const Root = styled.div`
-  width: 560px;
-  margin: 0 auto;
-  & .todo-input-wrapper {
-    margin-bottom: 10px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  & .todo-list {
-    width: 500px;
-    padding: 30px;
-    border-radius: 10px;
-    background-color: #f5f5f5;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: start;
-  }
-
-  & .logout {
-    width: 100%;
-    button {
-      margin: 0 auto;
-    }
-  }
-`;
